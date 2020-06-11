e33d655a9cd261f82803150bab4e091780df7bce
Josh Chernoff <jchernoff@morphic.pro>
2020-06-10 22:42:14 -0700

    serve true

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,9 @@ use Mix.Config
 # before starting your production server.
 config :elixirgroup, ElixirgroupWeb.Endpoint,
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true
+
 
 # Do not print debug messages in production
 config :logger, level: :info
